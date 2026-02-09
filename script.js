4b79947af721e479af9281be7ff0bc703897a7a3
bhanuprabha16bd-web <bhanuprabha16bd@gmail.com>
2026-02-09 23:16:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+// let count;
+
+// count = 10;
+
+// count = 20;
+
+// console.log(count);
+ 
+// var d;
+
+// d = "message"
+
+// message = "HI";
+
+// var message = "Hi all";
+
+// console.log(message);
+
+// const value = 4;
+
+// value = 5; //showing error (reassing is not allowed)
+
+// console.log(value);
+
+// Create variables
+// var city = "Chennai";
+// let temperature = 30;
+// const country = "India";
+
+// Reassign only allowed variables
+// city = "Bangalore";        // var can be reassigned
+// temperature = 32;          // let can be reassigned
+// country = "USA";        const cannot be reassigned
+
+// Print all values
+// console.log("City:", city);
+// console.log("Temperature:", temperature);
+// console.log("Country:", country);
+
+// var info = "Welcome";
+
+// console.log(info);
+
+// alert(info);
+
+// document.writeln(info);
+
+// let userchoice = confirm("do you want to continue");
+// console.log(userchoice);
+
+// let contactNo = prompt("enter your contact Number");
+// console.log(contactNo);
+// document.writeln(contactNo);
+
+// let update = "loading";
+
+// console.log(update);
+// console.warn(update);
+// console.error(update);
+
+// var price = 50;
+// console.log("Before:", price);
+// console.log("after:", price);
+
+// let value = true;
+// value = false;
+// console.log(value);
+
+// let message = "Hello";
+// console.log("First value:", message);
+// message = "Welcome";
+// console.log("Second value:", message);
+
+// var item = 10;
+// var item = 20;
+// var item = 50;
+// console.log(item);
+
+// const person = {
+//     name: "Anu",
+//     age: 25
+// };
+
+// person.age = 30; //modified property
+// console.log(person);
+
+var userName = "Rahul";
+let age = 28;
+const country = "India";
+console.log("Name:", userName);
+console.warn("Age:", age);
+console.error("Country:", country);
